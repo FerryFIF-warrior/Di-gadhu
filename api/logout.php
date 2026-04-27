2f4f7300800f3d0754265c1746b7ce778d925686
FIFYLI <ferry_fifyli8@student.uns.ac.id>
2026-04-27 19:31:11 +0700

    Update logout.php
    update function logout

--- api/logout.php
+++ api/logout.php
@@ -1,7 +1,25 @@
 <?php
 session_start();
+
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+  $params = session_get_cookies_params();
+  setcookie(
+    session_name(),
+    '',
+    time() - 42000,
+    $params["path"],
+    $params["domain"],
+    $params["secure"],
+    $params["httponly"]
+  );
+}
+
 session_destroy();
+
 setcookie("user_login", "", time() - 3600, "/");
+
 header("Location: login.php");
 exit();
 ?>
