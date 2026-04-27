fe02be39c1d71c1e02f234d3838d732b4c3e9677
FIFYLI <ferry_fifyli8@student.uns.ac.id>
2026-04-27 20:25:39 +0700

    Update koneksi.php
    implementasi session handler

--- api/koneksi.php
+++ api/koneksi.php
@@ -6,22 +6,23 @@ $password   = getenv('DB_PASSWORD') ?: '';
 $dbname     = getenv('DB_NAME') ?: 'DiGadhu';
 
 $conn = null;
-
 $conn = mysqli_init();
 if ($conn) {
     
     $conn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
-    
     $flags = MYSQLI_CLIENT_SSL;
-    
     if (!$conn->real_connect($servername, $username, $password, $dbname, $port, null, $flags)) {
         error_log("Koneksi database gagal: " . $conn->connect_error);
         $conn = null;
     } else {
         $conn->set_charset("utf8mb4");
-        
-        error_log("Koneksi ke TiDB Cloud BERHASIL (SSL, skip hostname verification)");
+        error_log("Koneksi ke TiDB Cloud BERHASIL (SSL)");
     }
 }
 
+if ($conn) {
+    require_once __DIR__ . '/session_hendler.php';
+    $handler = new MySQLSessionHandler($conn);
+    session_set_save_handler($handler, true);
+}
 ?>
