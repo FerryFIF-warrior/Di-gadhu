bbce6ff11f4d54fc01498d4e4df02015ab0ead42
FIFYLI <ferry_fifyli8@student.uns.ac.id>
2026-04-28 11:15:28 +0700

    Update admin_api.php
    implementasi cookie

--- api/admin_api.php
+++ api/admin_api.php
@@ -1,5 +1,13 @@
 <?php
 require_once 'koneksi.php';
+
+session_set_cookie_params([
+                          'path' => '/',
+                          'domain' => '.vercel.app',
+                          'secure' => true,
+                          'httponly' => true,
+                          'samesite' => 'Lax'
+]);
 session_start();
 
 header('Content-Type: application/json');
